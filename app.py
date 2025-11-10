672dcb03b365d7105222060efcc4da50d2234673
Sanjay <attellisanjay29@gmail.com>
2025-11-10 21:00:56 +0530

    first commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ with open(file_path, 'r', encoding='utf-8') as file:
 
 # ====== Step 2: Wait for 5 seconds so you can move your cursor ======
 print("You have 5 seconds to place the cursor where you want the text to be typed...")
-time.sleep(10)it
+time.sleep(10)
 
 # ====== Step 3: Start typing the content exactly as it is ======
 pyautogui.write(content, interval=0.01)  # interval controls typing speed (seconds per character)
